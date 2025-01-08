505e610d1f0042cf7f612b374d0cb82fb3838d4c
Florian Rappl <florian.rappl@smapiot.com>
2025-01-08 16:16:36 +0100

    Improved build

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@
 PID=`./platform.sh`
 
 rm -rf src/NetPack/bin/Release/net8.0/$PID/publish
-dotnet publish -r $PID -c Release src/NetPack/NetPack.csproj
-cp -r src/NetPack/bin/Release/net8.0/$PID/publish src/npm/@netpack/$PID/bin
+dotnet publish -r $PID -c Release -p:DebugType=None -p:DebugSymbols=false src/NetPack/NetPack.csproj
+
+rm -rf src/npm/@netpack/$PID/bin
+cp -r src/NetPack/bin/Release/net8.0/$PID/publish/ src/npm/@netpack/$PID/bin
 
 cd src/npm/dev
 npm i
